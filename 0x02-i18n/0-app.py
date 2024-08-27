af2866260992858288499b95d83d87a2392da7c7
ismailouzy <ismailouzal@gmail.com>
2024-08-27 23:35:11 +0000

    d

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,20 +1,18 @@
 #!/usr/bin/env python3
 """
-Basic Flask app
+basic flask app
 """
 
-
 from flask import Flask, render_template
-
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index(): -> str:
-    """render a html template"""
-    return render_template('0-index.html')
+def index():
+    """ render a html template"""
+    return render_template("0-index.html",)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
